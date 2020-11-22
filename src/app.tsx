f41a366ad2b8a42ec654ecbd9760c15b028a40d4
Ilya Kozlov <ilya@kozlov.dev>
2020-11-22 23:25:42 +0600

    feat(app): add increase, decrease and counter

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,24 @@
 import React, {ReactElement} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Button} from 'components/button/button';
+import {RootState} from './store/store';
+import {actions} from './store/actions';
 
 export function App(): ReactElement {
+  const dispatch = useDispatch();
+  const count = useSelector((state: RootState) => state.counter.count);
+  const increase = (step: number) => {
+    dispatch(actions.counter.increase(step));
+  };
+  const decrease = (step: number) => {
+    dispatch(actions.counter.decrease(step));
+  };
   return (
     <div className="app" data-test="app-component">
-      <span data-test="app-counter">Counter</span>
+      <h1>Counter</h1>
+      <p data-test="app-counter">{count}</p>
+      <Button label="Increase" onClick={() => increase(1)} data-test="increase-button" />
+      <Button label="Decrease" onClick={() => decrease(1)} data-test="decrease-button" />
     </div>
   );
 }
